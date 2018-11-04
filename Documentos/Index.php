73f64d5408767d378e012aef32b731024018a366
Unknown <romulocad36@gmail.com>
2018-11-04 11:41:56 -0200

    os 3 arquivos
    Max, aqui está os 3 arquivos que fiquei de enviar pra ti, o banner do projeto, o projeto feito no netbeans e também o documento em docx da monografia do projeto "Web antropométrica" consegui fazer com que ele ficasse em 20 páginas, espero que tu consiga corrigi-lo, peço perdão por não ter enviado antes pois esta merda de cidade fez com que ontem eu ficasse sem energia, consegui terminar só agora essa bosta, ta faltando o resumo, a ultima parte da problemática, e também falta tu inserir as citações que estão baseadas em site, novamente, peço desculpas por não ter terminado, vlw flws

---
+++ Documentos/Index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exame Antropométrico</title>
+    </head>
+    <body>
+       <div>
+	<center><div class="register-t">Cadastro</div>
+	<div class="register-c">
+		<form method="post" action="sistema/ru.php">
+			<p>Nome</p>
+			<input type="text" name="nome">
+			<p>Idade</p>
+			<input type="Idade">
+			<p>Sexo</p>
+			<input type="sexo" name="sexo">
+			
+	</div>
+	<div class="register-e">
+            <div class="button"><a href="Slaughter.php">Slaughter</a></div>
+		<div class="button"><a href="Pollock.php">Pollock</a></div>
+	</form></div></center>
+
+</div>
+
+    </body>
+</html>
